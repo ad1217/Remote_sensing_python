Corrected VI averages to ignore nans, write header information to file

Vegetation_Indices.py
@@ -65,8 +65,9 @@ def vegetation_indices(root_dir, filename):
     envi.save_image(dirname + filename + "vegetation_indices.hdr", veg_indices, force=True, dtype=np.float32,
                     metadata={'band names': VIs.keys()})
 
-    #average value for each VI
-    avg_vi = {k: np.average(v) for k, v in VIs}
+    #average value for each VI (not working...)
+    avg_vi = {k: np.nanmean(v) for k, v in VIs.iteritems()}
     avg_vi["Filename"] = filename
+    print(avg_vi)
     return avg_vi
 

convert-to-refl_v2.py
@@ -9,15 +9,15 @@ from edit_header_info import *
 
 
 #import calibration file (spectralon #3)
-cal_file = np.loadtxt("D:/spectralon_num3_resample.txt", skiprows= 1)
+cal_file = np.loadtxt("D:/spectralon_num3_resample.txt", skiprows=1)
 
 #import avg. spectralon #3
-whiteref_file = np.loadtxt("D:/white-ref/avg-only_white_plaque3.txt", skiprows= 1)
+whiteref_file = np.loadtxt("D:/white-ref/avg-only_white_plaque3.txt", skiprows=1)
 
 #set working directory
 os.chdir("D:/indoor_plant_measurements_20180831/")
 #set image folder
-path = "high_nutrient_4_2018_08_31_15_04_01"
+path = "waterlogged_4_2018_08_31_14_57_47"
 path2 = "D:/indoor_plant_measurements_20180831/" + path
 
 

vegetation_mask.py
@@ -36,7 +36,8 @@ def vegetation_mask(root_dir, filename):
     #vegetation = binary_global * img
 
     #save image in new folder called veg-extract
-    os.makedirs(dirname + "/veg-extract")
+    if not os.path.isdir(dirname + "veg-extract"):
+        os.makedirs(dirname + "veg-extract")
     envi.save_image(dirname + "veg-extract/" + filename + "NDVI05.hdr", img, force=True, dtype=np.float32)
     get_header_file_radiance_conv(fpath, dirname + "veg-extract/" + filename + "NDVI05.hdr")
 